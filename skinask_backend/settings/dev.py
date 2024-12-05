bad6f01763deb3f51917ead78a741b14e135db8d
hasibuzzaman.tonmoy@quore.com <hasibuzzaman.tonmoy@quore.com>
2024-12-04 20:09:45 -0600

    Initial commit

---
+++ skinask_backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*#-3e-9ob2j-y%z-fxj8t2nee#tvas-8u5ihec_gq7_4u-@qk&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
